c3eab7165c5ac85ff86dcf9e9b36ab872befba04
Kevin <kai.song@qiaofang.info>
2019-07-25 17:21:14 +0800

    add

--- src/redux/login/reducer.tsx
+++ src/redux/login/reducer.tsx
@@ -5,9 +5,10 @@ const totalState = {
 }
 
 export default function(state = totalState, action) {
+  console.log(action)
   switch (action.type) {
     case 'login_success':
-      return { loginStatus: Object.assign({ isLogin: true }, action.data) }
+      return action.data
     case 'login_failed':
       return { loginStatus: Object.assign({ isLogin: false }, action.data) }
     default:
